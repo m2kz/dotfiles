e19a0240d30bd5481db9e3a94976ba6b4288c453
Michał Karczewski <michal.karczewsky@gmail.com>
2019-02-07 15:30:56 +0100

    add i3 move script

--- scripts/i3move.py
+++ scripts/i3move.py
@@ -7,4 +7,4 @@ workspaces = json.loads(output)
 
 next_num = next(i for i in range(1, 100) if not [ws for ws in workspaces if ws['num'] == i])
 
-subprocess.call(['i3-msg', 'workspace number %i' % next_num])
+subprocess.call(['i3-msg', 'move container to workspace %i' % next_num])
