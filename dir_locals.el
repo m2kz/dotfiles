bd563a3c46f5f38c00b83082b75c15d58362c3cc
Michał Karczewski <michal.karczewsky@gmail.com>
2019-03-23 14:52:26 +0100

    add dir_locals.el

---
+++ dir_locals.el
@@ -0,0 +1,8 @@
+((nil .
+      ((cmake-ide-project-dir . "~/work/sasnet")
+       (cmake-ide-build-dir . "~/work/sasnet/build")
+       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON")
+       (helm-make-build-dir . "build")
+       (helm-make-arguments . "-j4")
+       (helm-ctest-dir . "~/work/sasnet/build/")
+       )))
